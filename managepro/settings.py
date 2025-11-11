4186fe3f8dddb2cd88b743dfbfcc070d79827764
Mayowa dabiri <mayowad43@gmail.com>
2025-11-11 16:56:13 +0100

    bug fixes

--- managepro/settings.py
+++ managepro/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = environ.get("DEBUG", "False") == "True"
+DEBUG = environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = environ.get("ALLOWED_HOST", "").split(",")
 
